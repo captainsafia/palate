e52a1382bd1a277a9ae4f740e28f8fc8d91127e9
mikiovsh <mikiovshievich@gmail.com>
2017-02-08 23:37:27 -0600

    fixed buttons

--- index.js
+++ index.js
@@ -3,4 +3,17 @@ window.onload = function(){
   document.getElementById("more").onclick = function() {
       $('.ui.modal').modal('show');
   };
+
+  document.getElementById("more2").onclick = function() {
+      $('.ui.modal').modal('show');
+  };
+
+  document.getElementById("more3").onclick = function() {
+      $('.ui.modal').modal('show');
+  };
+
+  document.getElementById("more4").onclick = function() {
+      $('.ui.modal').modal('show');
+  };
+
 }
