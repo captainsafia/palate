47b2231585b11d144792a85e2a2dce11fd9bf2f2
mikiovsh <mikiovshievich@gmail.com>
2017-02-12 16:06:56 -0600

    text changes, menu fixes and message addon

--- index.js
+++ index.js
@@ -16,4 +16,13 @@ window.onload = function(){
       $('.ui.modal').modal('show');
   };
 
+  $('.message .close')
+    .on('click', function() {
+      $(this)
+        .closest('.message')
+        .transition('fade')
+      ;
+    })
+  ;
+
 }
