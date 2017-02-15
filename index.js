01ee01201004f62eb2e42a61a90bf598832455d6
Safia Abdalla <safia@safia.rocks>
2017-02-15 17:59:13 -0600

    Fixed indent on JavaScript

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-
-
-
-
 window.onload = function() {
-
-
-
   $('.card').on('click', function() {
     $('.ui.modal').modal('show');
   });
-
-
-    $('.message .close').on('click', function() {
-        $(this)
-            .closest('.message')
-            .transition('fade');
-    });
-
+  $('.message .close').on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade');
+  });
 }
