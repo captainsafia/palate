15cf7c4323ba89e872a27e7abbb15248f95e04b3
ovsh <mikiovshievich@gmail.com>
2017-02-15 17:49:45 -0600

    recommendations

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
+
+
+
+
 window.onload = function() {
 
-    document
-        .getElementById("more")
-        .onclick = function() {
-        $('.ui.modal').modal('show');
-    };
 
-    document
-        .getElementById("more2")
-        .onclick = function() {
-        $('.ui.modal').modal('show');
-    };
 
-    document
-        .getElementById("more3")
-        .onclick = function() {
-        $('.ui.modal').modal('show');
-    };
+  $('.card').on('click', function() {
+    $('.ui.modal').modal('show');
+  });
 
-    document
-        .getElementById("more4")
-        .onclick = function() {
-        $('.ui.modal').modal('show');
-    };
 
     $('.message .close').on('click', function() {
         $(this)
