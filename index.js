a3e52bae7aeba2885f92da3e72d07fe2a0320c61
mikiovsh <mikiovshievich@gmail.com>
2017-02-25 20:14:02 -0600

    form page

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 window.onload = function() {
 
-  $('.card').on('click', function() {
-    $('.ui.modal').modal('show');
-  });
+    $('.card').on('click', function() {
+        $('.ui.modal').modal('show');
+    });
 
-  $('.message .close').on('click', function() {
-    $(this)
-      .closest('.message')
-      .transition('fade');
-  });
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade');
+    });
 
+    $('selection.dropdown').dropdown();
+
+    $('.ui.dropdown').dropdown({allowAdditions: true});
+
+    $('.ui.checkbox').checkbox();
+
+    document.getElementById("current-date").valueAsDate = new Date();
+
+}
+
+function submitForm() {
+    var frm = document.getElementById('dishform');
+
+    frm.reset(); // Reset
+    $(".ui.form").addClass('success');
+    return false; // Prevent page refresh
 
 }
