805b021acab5faa95f4b0fd62e923d1b1e857f76
mikiovsh <mikiovshievich@gmail.com>
2017-02-26 18:13:55 -0600

    form fix + text changes

---
+++ assets/form.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+
+    $('selection.dropdown').dropdown();
+
+    $('.ui.dropdown').dropdown({allowAdditions: true});
+
+    $('.ui.checkbox').checkbox();
+
+    if ($("#range-1").length > 0) {
+        $('#range-1').range({min: 0, max: 10, start: 5});
+    }
+    document.getElementById("current-date").valueAsDate = new Date();
+
+}
+
+function submitForm() {
+    var frm = document.getElementById('dishform');
+
+    frm.reset(); // Reset
+    $(".ui.form").addClass('success');
+    return false; // Prevent page refresh
+
+}
--- index.js
+++ index.js
@@ -12,17 +12,4 @@ window.onload = function() {
 
     $('.ui.dropdown').dropdown({allowAdditions: true});
 
-    $('.ui.checkbox').checkbox();
-
-    document.getElementById("current-date").valueAsDate = new Date();
-
-}
-
-function submitForm() {
-    var frm = document.getElementById('dishform');
-
-    frm.reset(); // Reset
-    $(".ui.form").addClass('success');
-    return false; // Prevent page refresh
-
 }
