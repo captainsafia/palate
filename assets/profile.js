2fc7e8d61375f11e7ea0a517b7b85b35ffa65eac
Safia Abdalla <safia@safia.rocks>
2017-03-08 05:56:54 -0600

    Add radio buttons and graphs for bar graph

--- assets/profile.js
+++ assets/profile.js
@@ -1,7 +1,14 @@
 $('.ui.radio.checkbox').checkbox();
 
-$('input[type="radio"]').on('click change', function(e) {
+$('#flavor-by-time input[type="radio"]').on('click change', function(e) {
+  console.log(e);
   var time = e.target.getAttribute('id');
   var path = 'images/crop_' + time + 'radar.png';
   $('#flavor-by-time-image img').attr('src', path);
+});
+
+$('#flavor-by-day input[type="radio"]').on('click change', function(e) {
+  var flavor = e.target.getAttribute('id');
+  var path = 'images/' + flavor + 'bars.png';
+  $('#flavor-by-day-image img').attr('src', path);
 });
